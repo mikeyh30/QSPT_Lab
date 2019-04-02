4fdf5a1fbe3f753ffb110fac19bd9e4a7e1358f9
Michael Hynes <mhynes@lcn.ucl.ac.uk>
2019-04-02 16:48:28 +0100

    Complete quantum state tomography. Add two (useless) functions

--- densityMat.m
+++ densityMat.m
@@ -1,7 +1,10 @@
-function [ output_args ] = densityMat( rx, ry, rz)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ rho ] = densityMat( rx, ry, rz)
+%densityMat outputs the density matrix for the given inputs
+    sigma0 = [1,0;0,1];
+    sigmaX = [0,1;1,0];
+    sigmaY = [0,-1i;1i,0];
+    sigmaZ = [1,0;0,-1];
+    
+    rho = 0.5*(sigma0+rx*sigmaX+ry*sigmaY+rz*sigmaZ);
 end
 
